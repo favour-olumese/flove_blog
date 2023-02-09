0d9849ea5483dae7e232db23b1477d040879b359
favour-olumese <olumesefavor@gmail.com>
2023-02-09 15:59:31 +0100

    Worked on the Writer and Article Models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import Writer, Article
 
 # admin.site.register(User, UserAdmin)
+
+admin.site.register(Writer)
+
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ['writer', 'title', 'text', 'article_img', 'pub_date', 'update_date']
+    readonly_fields = ['pub_date', 'update_date']  # For non-editable fields
+
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,27 +2,41 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
-class BlogUser(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Writer(models.Model):
+    """Class containing writer's information."""
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     profile_picture = models.ImageField(upload_to='user_img', null=True, blank=True)
+    bio = models.TextField(null= True, blank=True)
+    # TODO: Add field for links to social media pages.
+
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
 
     def __str__(self):
         return f'{self.last_name} {self.first_name}'
 
 
-class Articles(models.Model):
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    ) # https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#referencing-the-user-model
+class Article(models.Model):
+    writer = models.ForeignKey(Writer, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     text = models.TextField()
     article_img = models.ImageField(upload_to='article_img', null=True, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True) 
+    update_date = models.DateTimeField(auto_now=True)
     # article_tag = models.CharField(max_length=100)
 
+
+    class Meta:
+        ordering = ['-update_date']
+
+
     def __str__(self):
-        return self.title
+        return f'{self.title} by {self.writer}'
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     pass
