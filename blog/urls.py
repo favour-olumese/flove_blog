16631a659fbd9f82960cb7d3decc98e22310e38d
favour-olumese <olumesefavor@gmail.com>
2023-02-11 20:07:09 +0100

    Worked on views, templates, and urls for updating and deleting articles

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('articles/', views.ArticleListView.as_view(), name='articles'),
+    path('myarticles/', views.ArticleListView.as_view(), name='my-articles'),
     path('article/<int:pk>', views.ArticleDetailView.as_view(), name='article-detail'),
     path('article/create', views.ArticleCreateView.as_view(), name='new-article'),
+    path('article/<int:pk>/update', views.ArticleUpdateView.as_view(), name='update-article'),
+    path('article/<int:pk>/delete', views.ArticleDeleteView.as_view(), name='delete-article'),
     path('writer/create', views.WriterCreateView.as_view(), name='writer'),
     path('register/', views.register_user, name='register'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserRegistrationForm
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from django_email_verification import send_email
 from django.views.generic import ListView, DetailView
 from blog.models import Article, Writer
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin,  UserPassesTestMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 
@@ -60,6 +60,14 @@ def register_user(request):
 class ArticleListView(ListView):
     model = Article
 
+class MyArticleListView(ListView):
+    # queryset = Article.objects.filter(user=self.request.user)
+    template_name = 'blog/writer_articles.html'
+    context_object_name = 'my_articles'
+
+    def get_queryset(self):
+        return Article.objects.filter(user=self.request.user)
+
 
 class ArticleDetailView(DetailView):
     model = Article
@@ -72,9 +80,34 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         """Method to assign article to current user."""
-        written_by = Writer.objects.filter(user=self.request.user)[0]
-        form.instance.writer = written_by
+        article_writer = Writer.objects.filter(user=self.request.user)[0]
+        form.instance.writer = article_writer
         return super().form_valid(form)
+
+
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    queryset = Article.objects.all()
+    fields = ['article_img', 'title', 'text']
+
+    def test_func(self):
+        """"Works with UserPassesTestMixin.
+        Returns True when current user is the artticle onwer;
+        but returns False when current user is not the article onwer.
+        """
+
+        article = Article.objects.filter(id=self.kwargs['pk'])[0]
+        return article.writer.user == self.request.user
+
+    def handle_no_permission(self, *kwargs):
+        """Redirect current user who is not article owner to the article page.
+        By default, test_func redirects user who do not own the article to the 403 page.
+        """
+        
+        return redirect(reverse_lazy('article-detail', kwargs={'pk': self.kwargs['pk']}))
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+    model = Article
+    success_url = reverse_lazy('articles')
 
 
 class WriterCreateView(LoginRequiredMixin, CreateView):
