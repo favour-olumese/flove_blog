19b2382126cbace9684183c2d4ae3df8aeaa23c9
favour-olumese <olumesefavor@gmail.com>
2023-04-25 22:37:05 +0100

    Worked on buttons that hide and display reply box
    Joshua B. Adeniyi and Samuel Oketola helped me write the JavaScript that
    would add the textboxs and display them when the buttons are clicked. I
    continues by write the part that would hide the buttons when clicked but
    display them when their alternate buttons are clicked.
    
    Seeking for help when help is needed is helpful.

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,57 @@
-function show_reply_box() {
-    document.getElementsByClassName('reply-box').style.height="60px"
-    document.getElementsByClassName('reply-box').style.display="block"
-    document.getElementById('show').style.display="none";
-}
+var form = document.getElementsByClassName('reply-box');
+let btnShow= document.querySelectorAll('#show')
+let hideForm=document.querySelectorAll('#cancel-reply')
 
-function hide_reply_box() {
-    document.getElementsByClassName('reply-box').style.height="0px"
-    document.getElementsByClassName('reply-box').style.display="none"
-}
+btnShow.forEach((ele,i)=>{
+    ele.addEventListener("click",  ()=> {     
+        // form[i].style.height="60px"
+        form[i].style.display="block"
+        btnShow[i].style.display="none"
+        hideForm[i].style.display="block"
+    })
+})
+hideForm.forEach((ele,i)=>{
+    ele.addEventListener("click",  ()=> {     
+        form[i].style.display="none"
+        btnShow[i].style.display="block"
+        hideForm[i].style.display="none"
+    })
+})
+
+
+  
+    // ,
+
+
+    // function hide_reply_box() {
+    //     document.getElementsByClassName('reply-box')[i].style.height="0px"
+    //     document.getElementsByClassName('reply-box')[i].style.display="none"
+    //     document.getElementById('show').style.display="block";
+    // }
+    
+
+
+// console.log(btn)
+
+// var btn = document.getElementsByClassName('reply-box');
+
+// console.log(btn);
+// for (var i = 0; i < btn.length; i++) {
+//     console.log(btn[i]).style["height"]="20px";
+// }
+
+
+
+
+// function show_reply_box() {
+//     document.getElementsByClassName('reply-box')[0].style.height="60px"
+//     document.getElementsByClassName('reply-box')[0].style.display="block"
+//     document.getElementById('show').style.display="none";
+// }
+
+
+// function hide_reply_box() {
+//     document.getElementsByClassName('reply-box')[0].style.height="0px"
+//     document.getElementsByClassName('reply-box')[0].style.display="none"
+//     document.getElementById('show').style.display="block";
+// }
